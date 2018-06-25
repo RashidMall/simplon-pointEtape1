2effc61e5cb28376a73006313461dd73aa69e2ed
RashidMall <mr.mallaev@gmail.com>
2018-06-25 16:36:06 +0200

    The end

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const express = ('express');
-const bodyParser = ('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const url = 'myDataBase';
 
 let app = express();
 
 app.get('/', (req, res) => {
-    res.render('index.html');
+    res.render('./index.html', {}); //pff
 });
 
 // Show document's description
 app.get('/documents', (req, res) => {
 
 })
+
+app.use((req, res, next) => {
+    res.redirect('/');
+});
 
 app.listen(8080);
