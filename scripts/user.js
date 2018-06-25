c93db94b94c30ba621a185996718eb2d7ec1a1ce
RashidMall <mr.mallaev@gmail.com>
2018-06-25 10:28:57 +0200

    Create user class

---
+++ scripts/user.js
@@ -0,0 +1,14 @@
+class User {
+    constructor(u_name, fname, lname, passw, email, phone){
+        this.u_name = u_name;
+        this.fname = fname;
+        this.lname = lname;
+        this.passw = passw;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    getUserName(){
+        
+    }
+}
